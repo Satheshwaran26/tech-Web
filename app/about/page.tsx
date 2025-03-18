7f7f251c9919656ae54708306bd0d9ec56eb5a7b
Satheshwaran V <satheswaran6677@gmail.com>
2025-03-18 11:30:11 +0530

    Remove unused imports in AboutPage component for cleaner code

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -1,7 +1,7 @@
 "use client";
 import Image from "next/image";
 import { motion } from "framer-motion";
-import { useEffect, useState } from "react";
+
 import { FaGlobe, FaMobile, FaLaptopCode, FaLinkedin, FaTwitter, FaGithub } from 'react-icons/fa';
 const AboutPage = () => {
     return (
